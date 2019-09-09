cb6e659ecd0e4434338ebb31e8cfc95a8b349446
sumukha1994 <sumukharadhakrishna@gmail.com>
2019-09-08 23:53:49 -0700

    house robber dp

--- house_robber.py
+++ house_robber.py
@@ -25,6 +25,24 @@ class Solution(object):
 
         return max(ptr1, ptr2)
 
+    def dpSolution(self, nums):
+
+        if len(nums)==0:
+            return 0
+        if len(nums)==1:
+            return nums[0]
+        if len(nums)<=2:
+            return nums[0] if nums[0]>nums[1] else nums[1]
+
+        dp_array=[0 for i in range(len(nums))]
+        dp_array[0]=nums[0]
+        dp_array[1]=nums[1] if nums[1]>nums[0] else nums[0]
+        
+        for i in range(2, len(nums)):
+            dp_array[i]=max(dp_array[i-1], dp_array[i-2]+nums[i])
+            
+        return dp_array[len(nums)-1]
+
 if __name__ == "__main__":
     s = Solution()
     print(s.rob([2,7,9,3,1]))
