a96090d7531b49a5add4973d5b2d62cb06c26632
sumukha1994 <sumukharadhakrishna@gmail.com>
2019-09-13 19:05:27 -0700

    longest valid paranthesis (stack and dp)

--- 32_longest_valid_paranthesis.py
+++ 32_longest_valid_paranthesis.py
@@ -9,12 +9,12 @@ class Solution(object):
         maxlen=0
 
         for i, char in enumerate(s):
-        	if char=='(':
-        		stack.push(i)
-        	else:
-        		stack.pop()
-        		if len(stack)==0:
-        			stack.push(i)
-        		else:
-        			maxlen=max(maxlen, i-stack[len(stack)-1])
+            if char=='(':
+                stack.append(i)
+            elif len(stack)!=0:
+                stack.pop()
+                if len(stack)==0:
+                    stack.append(i)
+                else:
+                    maxlen=max(maxlen, i-stack[len(stack)-1])
         return maxlen
