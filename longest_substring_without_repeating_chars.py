44043f0d9138ca2a0bd73c41716c2af7868a4f3a
sumukha1994 <sumukharadhakrishna@gmail.com>
2019-09-18 10:15:54 -0700

    problems on strings

--- longest_substring_without_repeating_chars.py
+++ longest_substring_without_repeating_chars.py
@@ -12,6 +12,29 @@ class Solution(object):
                 max_length = len(longest_sub)
         return max_length
 
+        """ using hashmap
+        max_length=0
+        mydict={}
+        local_length=0
+        position=0
+        for i, character in enumerate(s):
+            if mydict.get(character, -1)==-1:
+                mydict[character] = i
+                local_length+=1
+            else:
+                if mydict.get(character)<position:
+                    mydict[character]=i
+                    local_length+=1
+                else:                    
+                    position=mydict.get(character)
+                    local_length=i-position
+                    mydict[character]=i           
+            
+            max_length=max(max_length, local_length)
+        return max_length
+
+        """
+
 if __name__=="__main__":
     long_sub=Solution()
     print(long_sub.lengthOfLongestSubstring(" "))
